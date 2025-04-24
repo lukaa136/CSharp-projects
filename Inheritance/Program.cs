e739e847779093477ddad5e0f2c4da85d467956b
Luca <avilaluca61@gmail.com>
2025-04-24 15:57:33 -0300

    Inheritance - PLay with the classes in program.cs

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Student s1 = new Student("Pablo", 16, 'B');
+        Student s2 = new Student("Marta", 13, 'A');
+        Student s3 = new Student("Juan", 10, 'C');
+        Teacher t1 = new Teacher("Jorge", 43, "Math");
+        Teacher t2 = new Teacher("Maria", 27, "History");
+
+        List<Person> persons = new List<Person>();
+
+        persons.Add(s1);
+        persons.Add(s2);
+        persons.Add(s3);
+        persons.Add(t1);
+        persons.Add(t2);
+
+        foreach (Person person in persons)
+        {
+            if (person.IsAdult())
+                Console.WriteLine("Adult");
+            else
+                Console.WriteLine("Minor");
+
+            person.Introduce();
+            Console.WriteLine();
+        }
+
+    }
+}
