d5e7be78e7a8a71f1527f1693829387500c87114
Luca <avilaluca61@gmail.com>
2025-04-24 15:36:24 -0300

    Inheritance - Add basic class functionalities

---
+++ Inheritance/class.cs
@@ -0,0 +1,45 @@
+public class Person
+{
+
+    //Properties & fields
+    public int Age
+    { get; private set; }
+
+    public string Name
+    { get; private set; }
+
+    //Constructors
+    public Person(string name, int age)
+    {
+        Age = age;
+        Name = name;
+    }
+
+    //Methods
+    public virtual void Introduce()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+    }
+}
+
+public class Student : Person
+{
+    //Properties & fields
+    public char Grade
+    { get; private set; }
+
+    //Constructors
+    public Student(string name, int age, char grade) : base(name, age)
+    { 
+        Grade = grade;
+    }
+
+    //Methods
+    public override void Introduce()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Grade {Grade}");
+    }
+}
